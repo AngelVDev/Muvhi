73988f310c365c3a5e13c6c4a030ee0d35d8adc5
Chaos <antonioscutari2@hotmail.com>
2022-10-27 20:42:57 -0300

    Material Refactor

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import {
+  CardActionArea,
+  CardActions,
+  Collapse,
+  IconButton,
+} from "@mui/material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Link } from "react-router-dom";
+
+export default function MultiActionAreaCard({ id, title, text, poster }) {
+  const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card
+      sx={{
+        maxWidth: 325,
+        backgroundColor: "#3131319b",
+        color: "whitesmoke",
+        marginBottom: "1.5rem",
+      }}
+    >
+      <CardActionArea>
+        <Link style={{ textDecoration: "none" }} to={`/movie/${id}`}>
+          <CardMedia
+            component="img"
+            height="450"
+            image={poster}
+            alt={`poster+${id}`}
+          />
+        </Link>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{text ? text : "Loading..."}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,27 +1,42 @@
+import {
+  Avatar,
+  // Icon,
+  // IconButton,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeMovieFavourite } from "../../actions";
+// import FavoriteBorderSharpIcon from "@mui/icons-material/FavoriteBorderSharp";
 import "./Favorites.css";
 
 export class ConnectedList extends Component {
   render() {
     return (
       <div>
-        <h2>Películas Pedorras</h2>
+        <h2>This are your favourites</h2>
         <ul>
           {this.props.moviesFav?.length > 0 ? (
             this.props.moviesFav?.map((movie) => (
-              <li key={movie.imdbID}>
-                <Link to={`/movie/${movie.imdbID}`}>
-                  <span>{movie.Title}</span>
-                </Link>
+              <>
+                <ListItemAvatar>
+                  <Link to={`/movie/${movie.imdbID}`}>
+                    <Avatar
+                      alt={`Avatar n°${movie.imdbID + 1}`}
+                      src={movie.Poster}
+                      sx={{ width: 87, height: 87 }}
+                    />
+                  </Link>
+                </ListItemAvatar>
+                <ListItemText id={movie.imdbID} primary={`${movie.Title}`} />
                 <button
                   onClick={() => this.props.removeMovieFavourite(movie.imdbID)}
                 >
-                  X
+                  {/* <Icon>{FavoriteBorderSharpIcon}</Icon> */}
                 </button>
-              </li>
+              </>
             ))
           ) : (
             <h3>No hay películas aún</h3>
