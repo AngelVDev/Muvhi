104ca27dc676153b21e2c14234941c48cfab1936
Angel Scutari <antonioscutari2@hotmail.com>
2022-12-25 23:33:43 -0300

    refactor buscador

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -1,79 +1,61 @@
-import { Box, Button, Container, IconButton, Input } from "@mui/material";
-import React, { Component } from "react";
+import { Box, Button, Input } from "@mui/material";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addMovieFavourite, getMovies } from "../../actions";
 import MultiActionAreaCard from "../Cards/Card.jsx";
-import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Buscador.css";
 
-export class Buscador extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-    };
-  }
+const Buscador = ({ movies, getMovies }) => {
+  const [title, setTitle] = useState("");
 
-  handleChange(event) {
-    this.setState({ title: event.target.value });
-  }
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.getMovies(this.state.title);
-  }
-
-  render() {
-    const { title } = this.state;
-
-    return (
-      <Box>
-        <form className='form-container' onSubmit={(e) => this.handleSubmit(e)}>
-          <Input
-            type='text'
-            id='title'
-            autoComplete='off'
-            placeholder='Search here'
-            value={title}
-            onChange={(e) => this.handleChange(e)}
-          />
-          <Button
-            sx={{ backgroundColor: "darkgoldenrod", color: "white" }}
-            type='submit'
-          >
-            Search
-          </Button>
-        </form>
-        <ul>
-          {" "}
-          {this.props.movies &&
-            this.props.movies.map((movie) => (
-              <>
-                <MultiActionAreaCard
-                  id={movie.imdbID}
-                  poster={movie.Poster}
-                  title={movie.Title}
-                  text={movie.Plot}
-                  key={"div" + movie.imdbID}
-                />
-              </>
-            ))}
-        </ul>
-      </Box>
-    );
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    movies: state.moviesLoaded,
+    getMovies(title);
   };
-}
 
-function mapDispatchToProps(dispatch) {
-  return {
-    getMovies: (title) => dispatch(getMovies(title)),
-    addMovieFavourite: (movie) => dispatch(addMovieFavourite(movie)),
-  };
-}
+  return (
+    <Box>
+      <form className='form-container' onSubmit={handleSubmit}>
+        <Input
+          type='text'
+          id='title'
+          autoComplete='off'
+          placeholder='Search here'
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <Button
+          sx={{ backgroundColor: "darkgoldenrod", color: "white" }}
+          type='submit'
+        >
+          Search
+        </Button>
+      </form>
+      <ul>
+        {movies &&
+          movies.map((movie) => (
+            <>
+              <MultiActionAreaCard
+                id={movie.imdbID}
+                poster={movie.Poster}
+                title={movie.Title}
+                text={movie.Plot}
+                key={"div" + movie.imdbID}
+              />
+            </>
+          ))}
+      </ul>
+    </Box>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  movies: state.moviesLoaded,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getMovies: (title) => dispatch(getMovies(title)),
+  addMovieFavourite: (movie) => dispatch(addMovieFavourite(movie)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Buscador);
