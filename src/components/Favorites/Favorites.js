6a50831124ee391873ffc4752e23dd1713f067a9
Angel Scutari <antonioscutari2@hotmail.com>
2022-12-25 23:29:15 -0300

    refactor favorites

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,61 +1,45 @@
-import {
-  Avatar,
-  // Icon,
-  // IconButton,
-  ListItemAvatar,
-  ListItemText,
-} from "@mui/material";
-import React, { Component } from "react";
+import { Avatar, ListItemAvatar, ListItemText } from "@mui/material";
+import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeMovieFavourite } from "../../actions";
-// import FavoriteBorderSharpIcon from "@mui/icons-material/FavoriteBorderSharp";
 import "./Favorites.css";
 
-export class ConnectedList extends Component {
-  render() {
-    return (
-      <div>
-        <h2>This are your favourites</h2>
-        <ul>
-          {this.props.moviesFav?.length > 0 ? (
-            this.props.moviesFav?.map((movie) => (
-              <>
-                <ListItemAvatar>
-                  <Link to={`/movie/${movie.imdbID}`}>
-                    <Avatar
-                      alt={`Avatar n°${movie.imdbID + 1}`}
-                      src={movie.Poster}
-                      sx={{ width: 87, height: 87 }}
-                    />
-                  </Link>
-                </ListItemAvatar>
-                <ListItemText id={movie.imdbID} primary={`${movie.Title}`} />
-                <button
-                  onClick={() => this.props.removeMovieFavourite(movie.imdbID)}
-                >
-                  {/* <Icon>{FavoriteBorderSharpIcon}</Icon> */}
-                </button>
-              </>
-            ))
-          ) : (
-            <h3>No hay películas aún</h3>
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+const ConnectedList = ({ moviesFav, removeMovieFavourite }) => {
+  return (
+    <div>
+      <h2>This are your favourites</h2>
+      <ul>
+        {moviesFav?.length > 0 ? (
+          moviesFav.map((movie) => (
+            <>
+              <ListItemAvatar>
+                <Link to={`/movie/${movie.imdbID}`}>
+                  <Avatar
+                    alt={`Avatar n°${movie.imdbID + 1}`}
+                    src={movie.Poster}
+                    sx={{ width: 87, height: 87 }}
+                  />
+                </Link>
+              </ListItemAvatar>
+              <ListItemText id={movie.imdbID} primary={`${movie.Title}`} />
+              <button onClick={() => removeMovieFavourite(movie.imdbID)} />
+            </>
+          ))
+        ) : (
+          <h3>No hay películas aún</h3>
+        )}
+      </ul>
+    </div>
+  );
+};
 
-function mapStateToProps(state) {
-  return {
-    moviesFav: state.moviesFavourites,
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    removeMovieFavourite: (id) => dispatch(removeMovieFavourite(id)),
-  };
-}
+const mapStateToProps = (state) => ({
+  moviesFav: state.moviesFavourites,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  removeMovieFavourite: (id) => dispatch(removeMovieFavourite(id)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ConnectedList);
