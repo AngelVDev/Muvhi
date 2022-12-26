1ce38fcfcccfde0886c6706194945c7e8c9b9255
Angel Scutari <antonioscutari2@hotmail.com>
2022-12-25 23:29:26 -0300

    refactor details

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -5,50 +5,46 @@ import { getMovieDetail } from "../../actions/index";
 
 import "./Movie.css";
 
-class Movie extends React.Component {
-  componentDidMount() {
-    let id = this.props.match.params.id;
-    this.props.getMovieDetail(id);
-  }
-  render() {
-    return (
-      <div className="movie-card">
-        <h1 className="title">{this.props.movieDetail.Title}</h1>
-        <h2>Year: {this.props.movieDetail.Year}</h2>
-        <h2>Written by: {this.props.movieDetail.Writer}</h2>
-        <h2>Directed by: {this.props.movieDetail.Director}</h2>
-        <h2>Rated: {this.props.movieDetail.Rated}</h2>
-        <h2>Released: {this.props.movieDetail.Release}</h2>
-        <h2>Genre: {this.props.movieDetail.Genre}</h2>
-        <img src={this.props.movieDetail.Poster} alt="Nope.jpg" />
-        <h2>Actors: {this.props.movieDetail.Actors}</h2>
-        <h2>Plot: {this.props.movieDetail.Plot}</h2>
-        <>
-          <h2>Rating:</h2>
-          <Rating
-            name="customized-10"
-            precision={0.1}
-            defaultValue={0}
-            max={10}
-            value={this.props.movieDetail.imdbRating * 1}
-            readOnly
-          />
-          <h2>Language: {this.props.movieDetail.Language}</h2>
-        </>
-      </div>
-    );
-  }
-}
-function mapStateToProps(state) {
-  return {
-    movieDetail: state.movieDetail,
-  };
-}
+const Movie = ({ movieDetail, getMovieDetail, match }) => {
+  React.useEffect(() => {
+    let id = match.params.id;
+    getMovieDetail(id);
+  }, [getMovieDetail, match]);
 
-function mapDispatchToProps(dispatch) {
-  return {
-    getMovieDetail: (id) => dispatch(getMovieDetail(id)),
-  };
-}
+  return (
+    <div className='movie-card'>
+      <h1 className='title'>{movieDetail.Title}</h1>
+      <h2>Year: {movieDetail.Year}</h2>
+      <h2>Written by: {movieDetail.Writer}</h2>
+      <h2>Directed by: {movieDetail.Director}</h2>
+      <h2>Rated: {movieDetail.Rated}</h2>
+      <h2>Released: {movieDetail.Release}</h2>
+      <h2>Genre: {movieDetail.Genre}</h2>
+      <img src={movieDetail.Poster} alt='Nope.jpg' />
+      <h2>Actors: {movieDetail.Actors}</h2>
+      <h2>Plot: {movieDetail.Plot}</h2>
+      <>
+        <h2>Rating:</h2>
+        <Rating
+          name='customized-10'
+          precision={0.1}
+          defaultValue={0}
+          max={10}
+          value={movieDetail.imdbRating * 1}
+          readOnly
+        />
+        <h2>Language: {movieDetail.Language}</h2>
+      </>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  movieDetail: state.movieDetail,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getMovieDetail: (id) => dispatch(getMovieDetail(id)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Movie);
