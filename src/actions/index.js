75ca6edf7cd41e0e3f233914e02ab50ff3e23804
Angel <antonioscutari2@hotmail.com>
2023-01-04 22:12:48 -0300

    Added an action to always have movies in home

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,28 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "https://www.omdbapi.com/";
+const BASE_URL = 'https://www.omdbapi.com/';
 
 export function getMovies(title) {
   return function (dispatch) {
     return fetch(`${BASE_URL}?apikey=${API_KEY}&s=${title}`)
       .then((response) => response.json())
       .then((movies) => {
-        dispatch({ type: "GET_MOVIES", payload: movies });
+        dispatch({ type: 'GET_MOVIES', payload: movies });
       });
+  };
+}
+
+export function getMoviesForHome(year) {
+  return async function (dispatch) {
+    const response = await fetch(
+      `${BASE_URL}?apikey=${API_KEY}&s=This&y=${year}`
+    );
+
+    const newResponse = await fetch(
+      `${BASE_URL}?apikey=${API_KEY}&s=This&y=${year - 1}`
+    );
+    const movies =
+      response.length > 4 ? await response.json() : await newResponse.json();
+    dispatch({ type: 'GET_MOVIES', payload: movies });
   };
 }
 export function getMovieDetail(id) {
@@ -15,14 +30,14 @@ export function getMovieDetail(id) {
     return fetch(`${BASE_URL}?apikey=${API_KEY}&i=${id}&plot=full`)
       .then((response) => response.json())
       .then((detail) => {
-        dispatch({ type: "GET_MOVIE_DETAIL", payload: detail });
+        dispatch({ type: 'GET_MOVIE_DETAIL', payload: detail });
       });
   };
 }
 export function addMovieFavourite(payload) {
-  return { type: "ADD_MOVIE_FAVOURITE", payload: payload };
+  return { type: 'ADD_MOVIE_FAVOURITE', payload: payload };
 }
 
 export function removeMovieFavourite(payload) {
-  return { type: "REMOVE_MOVIE_FAVOURITE", payload: payload };
+  return { type: 'REMOVE_MOVIE_FAVOURITE', payload: payload };
 }
