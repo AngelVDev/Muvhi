9c3dee0713e4f532ef5c29758521df629127de51
Angel <antonioscutari2@hotmail.com>
2023-01-04 22:13:07 -0300

    Added the useEffect for the new Action

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -1,32 +1,39 @@
-import { Box, Button, Input } from "@mui/material";
-import React, { useState } from "react";
-import { connect } from "react-redux";
-import { addMovieFavourite, getMovies } from "../../actions";
-import MultiActionAreaCard from "../Cards/Card.jsx";
-import "./Buscador.css";
+import { Box, Button, Input } from '@mui/material';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
+import { addMovieFavourite, getMovies, getMoviesForHome } from '../../actions';
+import MultiActionAreaCard from '../Cards/Card.jsx';
+import './Buscador.css';
 
 const Buscador = ({ movies, getMovies }) => {
-  const [title, setTitle] = useState("");
-
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+  const date = new Date();
+  const thisYear = date.getFullYear();
   const handleSubmit = (event) => {
     event.preventDefault();
     getMovies(title);
   };
 
+  React.useEffect(() => {
+    dispatch(getMoviesForHome(thisYear));
+  }, [movies]);
+
   return (
     <Box>
-      <form className='form-container' onSubmit={handleSubmit}>
+      <form className="form-container" onSubmit={handleSubmit}>
         <Input
-          type='text'
-          id='title'
-          autoComplete='off'
-          placeholder='Search here'
+          type="text"
+          id="title"
+          autoComplete="off"
+          placeholder="Search here"
           value={title}
           onChange={(event) => setTitle(event.target.value)}
         />
         <Button
-          sx={{ backgroundColor: "darkgoldenrod", color: "white" }}
-          type='submit'
+          sx={{ backgroundColor: 'darkgoldenrod', color: 'white' }}
+          type="submit"
         >
           Search
         </Button>
@@ -40,7 +47,7 @@ const Buscador = ({ movies, getMovies }) => {
                 poster={movie.Poster}
                 title={movie.Title}
                 text={movie.Plot}
-                key={"div" + movie.imdbID}
+                key={'div' + movie.imdbID}
               />
             </>
           ))}
