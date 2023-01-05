45e85acf9a4492e415ec9450807dc1bb14b48024
Angel <antonioscutari2@hotmail.com>
2023-01-04 21:06:20 -0300

    added muvhi logo

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../logoHenry.png'
+import Logo from '../../logoHenry.svg';
 
 import './Navbar.css';
 
 export default function NavBar() {
-    return (
-        <header className="navbar">
-            <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </div>
-            <nav>
-                <ul className="list">
-                    <li className="list-item">
-                        <NavLink exact to="/" >Home</NavLink>
-                        <NavLink to="/favs" >Favoritas</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="navbar">
+      <div>
+        <img
+          id="logoHenry"
+          src={Logo}
+          width="50"
+          height="50"
+          className="d-inline-block align-top"
+          alt="muvhiLogo"
+        />
+      </div>
+      <nav>
+        <ul className="list">
+          <li className="list-item">
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/favs">Favoritas</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
