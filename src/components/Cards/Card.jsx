15d7808c2de1980cd4b911b16417e41f50c6619a
Angel Scutari <antonioscutari2@hotmail.com>
2023-02-17 03:22:21 -0300

    Collapse deleted

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -4,40 +4,15 @@ import { styled } from "@mui/material/styles";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import {
-  CardActionArea,
-  CardActions,
-  Collapse,
-  IconButton,
-} from "@mui/material";
+import { CardActionArea, CardActions, IconButton } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { addMovieFavourite } from "../../redux/actions";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-export default function MultiActionAreaCard({
-  id,
-  entity,
-  title,
-  text,
-  poster,
-}) {
-  const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
-    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  }));
-  const [expanded, setExpanded] = React.useState(false);
+export default function MultiActionAreaCard({ id, entity, title, poster }) {
+  console.log("ramen");
   const dispatch = useDispatch();
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Card
@@ -63,27 +38,12 @@ export default function MultiActionAreaCard({
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions disableSpacing>
-        <IconButton
-          onClick={() => dispatch(addMovieFavourite(entity))}
-          aria-label='add to favorites'
-        >
-          <FavoriteIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label='show more'
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{text ? text : "Loading..."}</Typography>
-        </CardContent>
-      </Collapse>
+      <IconButton
+        onClick={() => dispatch(addMovieFavourite(entity))}
+        aria-label='add to favorites'
+      >
+        <FavoriteIcon />
+      </IconButton>
     </Card>
   );
 }
