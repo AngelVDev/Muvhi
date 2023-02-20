1088d8cd367086f1bf36136386c89f30e1771419
Angel Scutari <antonioscutari2@hotmail.com>
2023-02-19 23:53:25 -0300

    svg to jsx

--- src/components/Footer/icons/FacebookIcon.jsx
+++ src/components/Footer/icons/FacebookIcon.jsx
@@ -4,18 +4,17 @@ const FacebookIcon = () => {
   return (
     <>
       <svg
+        xmlns='http://www.w3.org/2000/svg'
         width='24'
         height='24'
-        viewBox='0 0 24 24'
         fill='none'
-        xmlns='http://www.w3.org/2000/svg'
-        className='svg-icon svg-icon-facebook-logo'
+        viewBox='0 0 24 24'
       >
         <path
-          fill-rule='evenodd'
-          clip-rule='evenodd'
-          d='M13.9868 13.1621V21.9841H10.0418V13.1621H6.8418V9.51207H10.0468V6.73207C10.0468 3.56707 11.9318 1.82007 14.8148 1.82007C15.7616 1.83321 16.7061 1.91577 17.6408 2.06707V5.17307H16.0448C15.4952 5.10007 14.9422 5.28088 14.5419 5.66447C14.1415 6.04807 13.9373 6.59284 13.9868 7.14507V9.51207H17.4868L16.9278 13.1621H13.9868Z'
           fill='currentColor'
+          fillRule='evenodd'
+          d='M13.987 13.162v8.822h-3.945v-8.822h-3.2v-3.65h3.205v-2.78c0-3.165 1.885-4.912 4.768-4.912.947.013 1.891.096 2.826.247v3.106h-1.596a1.825 1.825 0 00-2.058 1.972v2.367h3.5l-.56 3.65h-2.94z'
+          clipRule='evenodd'
         ></path>
       </svg>
     </>
