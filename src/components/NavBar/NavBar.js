8dc5b4289a941d6d9c1f1c1e774e4ee0f6b8c0d2
Angel Scutari <antonioscutari2@hotmail.com>
2023-02-20 03:05:44 -0300

    cleaning

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import Logo from '../../logoHenry.svg';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../logoHenry.svg";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 export default function NavBar() {
   return (
-    <header className="navbar">
+    <header className='navbar'>
       <div>
-        <Link to={'/'}>
+        <Link to={"/"}>
           <img
-            id="logoHenry"
+            id='logoHenry'
             src={Logo}
-            width="50"
-            height="50"
-            className="d-inline-block align-top"
-            alt="muvhiLogo"
+            width='50'
+            height='50'
+            className='d-inline-block align-top'
+            alt='muvhiLogo'
           />
         </Link>
       </div>
       <nav>
-        <ul className="list">
-          <li className="list-item">
-            <NavLink exact to="/">
+        <ul className='list'>
+          <li className='list-item'>
+            <NavLink exact to='/'>
               Home
             </NavLink>
-            <NavLink to="/favs">Favoritas</NavLink>
+            <NavLink to='/favs'>Favourites</NavLink>
           </li>
         </ul>
       </nav>
